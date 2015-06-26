6ecd863b72b695070cbbd3a63db3f767bce06036
Benjamin Li <bli@riorey.com>
2015-06-26 16:36:46 -0400

    versioncmp specifies that arguments are strings.
    This change is required when using the future parser.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -25,7 +25,7 @@ class pulp::repo (
             }
         }
         'RedHat', 'CentOS': {
-            if versioncmp($::facterversion, 2.0) < 0 {
+            if versioncmp($::facterversion, '2.0') < 0 {
                 package { 'redhat-lsb':
                     ensure => 'installed'
                 }
